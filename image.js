d7ed2b61ca7985dccaba495b42cbc6bd40a8bfbc
Shashank03200 <shashank032000@gmail.com>
2020-11-10 00:34:32 +0530

    First commit

---
+++ image.js
@@ -0,0 +1,21 @@
+const https = require('https');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+
+module.exports =  async function (icon, description) {
+
+    let query = "";
+    const CLIENT_ID = "9hi8I_oLDrWv50uMPZ4EzCRt4Re0U_82DVaqB746DUQ";
+
+    if (icon.endsWith('d'))
+        query = description + " weather";
+    else if (icon.endsWith('n'))
+        query = description + " weather";
+        
+    const imgUrl = `https://api.unsplash.com/search/photos/?client_id=${CLIENT_ID}&query=${query}`;
+
+    const response = await fetch(imgUrl)
+    const json = await response.json();
+    return json;
+    
+}
